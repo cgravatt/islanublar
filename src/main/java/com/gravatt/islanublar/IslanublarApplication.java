c458ee4fd29f7bbfc9c99f9cf64e90cee0129205
CG <gravatt.christian@gmail.com>
2018-09-05 13:31:45 -0400

    Initial Commit

---
+++ src/main/java/com/gravatt/islanublar/IslanublarApplication.java
@@ -0,0 +1,12 @@
+package com.gravatt.islanublar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IslanublarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IslanublarApplication.class, args);
+	}
+}
